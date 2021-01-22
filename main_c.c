c0b9a0ecb5ebabe03487d89dd080887a43e623e1
= <=>
2021-01-22 11:07:35 +0000

    adding the second print help for the program

--- main_c.c
+++ main_c.c
@@ -7,34 +7,38 @@ int main(int argc, char **argv)
 {
 
 	if(argc < 3)
-		return 1;
-	int FileSrc=open(argv[1], O_RDONLY);
-
-	if (FileSrc == -1)
-
-		printf("there is no file called %s !!\n", argv[1] );
+		printf("too few arguments !\n");
 
 	else 
 	{
-	
+		int FileSrc=open(argv[1], O_RDONLY);
 
-		int FileDst=open(argv[2], O_CREAT | O_WRONLY, S_IRUSR);
+		if (FileSrc == -1)
 
-		if (FileDst == -1)
-			
-			printf("there is already file called %s!!\n", argv[2] );
+			printf("there is no file called %s !!\n", argv[1] );
 
-		else
+		else 
 		{
-			char buffer[2];
-			ssize_t charCount;
+		
 
-			while (  (charCount = read(FileSrc, buffer, sizeof(buffer))  ) ) // if the value is true :: !=0 
+			int FileDst=open(argv[2], O_CREAT | O_WRONLY, S_IRUSR);
+
+			if (FileDst == -1)
+				
+				printf("there is already file called %s!!\n", argv[2] );
+
+			else
 			{
-				write(FileDst, buffer, charCount);
+				char buffer[2];
+				ssize_t charCount;
 
+				while (  (charCount = read(FileSrc, buffer, sizeof(buffer))  ) ) // if the value is true :: !=0 
+				{
+					write(FileDst, buffer, charCount);
+
+				}
 			}
-		}
-	}	
+		}	
+	}
 
 }
